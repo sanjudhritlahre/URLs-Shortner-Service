612a61e032e1d2984494c3274201a2947abcef34
Sanju Dhritlahre <romeyoboy765@gmail.com>
2026-07-04 16:27:52 +0530

    Installing & Setting Up Drizzle ORM for Database

---
+++ models/users.models.js
@@ -0,0 +1,8 @@
+import { uuid, text, pgTable, varchar } from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable("users", {
+  id: uuid().primaryKey().defaultRandom(),
+  name: varchar({ length: 255 }).notNull(),
+  email: varchar({ length: 255 }).notNull().unique(),
+  password: text().notNull()
+});

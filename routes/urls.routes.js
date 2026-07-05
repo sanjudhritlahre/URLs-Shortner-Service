16f5e1cbb3c51156d1295a15cd6969cffe1f1c9a
Sanju Dhritlahre <romeyoboy765@gmail.com>
2026-07-06 03:53:56 +0530

    Fetching All Shortened URLs by the Logged-In User

--- routes/urls.routes.js
+++ routes/urls.routes.js
@@ -35,6 +35,15 @@ router.post("/shorten", ensureAuthenticated, async (req, res) => {
   });
 });
 
+router.get('/codes', ensureAuthenticated, async function (req, res) {
+  const codes = await db
+    .select()
+    .from(urlsTable)
+    .where(eq(urlsTable.userId, req.user.id));
+
+  return res.json({ codes });
+});
+
 router.get("/:shortCode", async (req, res) => {
   const code = req.params.shortCode;
 
